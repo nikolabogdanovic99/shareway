f87f5394f8feb1e9f152d191433f64bd1506a573
Nikola  Bogdanovic <bogdanik@students.zhaw.ch>
2025-11-25 19:12:33 +0100

    first commit

---
+++ src/main/java/ch/zhaw/shareway/SharewayApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.shareway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharewayApplication.class, args);
+	}
+
+}
