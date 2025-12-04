6f4a872fd85239ac7ce5d87818016175fe69643c
Nikola  Bogdanovic <bogdanik@students.zhaw.ch>
2025-12-04 12:44:36 +0100

    Add cascading dropdowns for vehicle make/model selection

--- frontend/src/routes/vehicles/+page.svelte
+++ frontend/src/routes/vehicles/+page.svelte
@@ -5,6 +5,34 @@
   let { user, isAuthenticated } = data;
 
   let myVehicles = $state(data.myVehicles);
+  let selectedMake = $state('');
+  let selectedModel = $state('');
+
+  // Car makes and models
+  const makes = ['Audi', 'BMW', 'Ford', 'Mercedes', 'Opel', 'Renault', 'Skoda', 'Tesla', 'Toyota', 'VW'];
+  
+  const models = {
+    'Audi': ['A1', 'A3', 'A4', 'A6', 'Q3', 'Q5', 'Q7', 'e-tron'],
+    'BMW': ['1er', '2er', '3er', '5er', 'X1', 'X3', 'X5', 'i3', 'i4'],
+    'Ford': ['Fiesta', 'Focus', 'Kuga', 'Mustang', 'Puma', 'Transit'],
+    'Mercedes': ['A-Klasse', 'B-Klasse', 'C-Klasse', 'E-Klasse', 'GLA', 'GLC', 'EQA', 'EQC'],
+    'Opel': ['Astra', 'Corsa', 'Crossland', 'Grandland', 'Mokka', 'Zafira'],
+    'Renault': ['Captur', 'Clio', 'Kadjar', 'Megane', 'Scenic', 'Twingo', 'Zoe'],
+    'Skoda': ['Fabia', 'Kamiq', 'Karoq', 'Kodiaq', 'Octavia', 'Superb', 'Enyaq'],
+    'Tesla': ['Model 3', 'Model S', 'Model X', 'Model Y'],
+    'Toyota': ['Auris', 'Aygo', 'C-HR', 'Corolla', 'RAV4', 'Yaris', 'Prius'],
+    'VW': ['Golf', 'ID.3', 'ID.4', 'Passat', 'Polo', 'T-Cross', 'T-Roc', 'Tiguan']
+  };
+
+  // Get models for selected make
+  const availableModels = $derived(selectedMake ? models[selectedMake] || [] : []);
+
+  // Reset model when make changes
+  $effect(() => {
+    if (selectedMake) {
+      selectedModel = '';
+    }
+  });
 
   // Update when data changes
   $effect(() => {
@@ -12,13 +40,6 @@
   });
 
   // Check if user is driver or admin
-  $effect(() => {
-    if (!isAuthenticated || !user.user_roles || 
-        (!user.user_roles.includes("driver") && !user.user_roles.includes("admin"))) {
-      // Could redirect, but for now just show message
-    }
-  });
-
   const isDriverOrAdmin = isAuthenticated && user.user_roles && 
     (user.user_roles.includes("driver") || user.user_roles.includes("admin"));
 </script>
@@ -46,11 +67,21 @@
     <div class="row mb-3">
       <div class="col-md-6">
         <label class="form-label" for="make">Make (Brand) *</label>
-        <input class="form-control" id="make" name="make" type="text" placeholder="e.g. Tesla" required />
+        <select class="form-select" id="make" name="make" bind:value={selectedMake} required>
+          <option value="">Select make...</option>
+          {#each makes as make}
+            <option value={make}>{make}</option>
+          {/each}
+        </select>
       </div>
       <div class="col-md-6">
         <label class="form-label" for="model">Model *</label>
-        <input class="form-control" id="model" name="model" type="text" placeholder="e.g. Model 3" required />
+        <select class="form-select" id="model" name="model" bind:value={selectedModel} required disabled={!selectedMake}>
+          <option value="">Select model...</option>
+          {#each availableModels as model}
+            <option value={model}>{model}</option>
+          {/each}
+        </select>
       </div>
     </div>
     <div class="row mb-3">
